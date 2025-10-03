9415e63bdc5a9795e3fe85c08e9eac318fa70527
Chndn <chndnsharma5036@gmail.com>
2025-10-04 01:51:19 +0530

    implementing express

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+    res.render("index");
 });
 
 // Start server
